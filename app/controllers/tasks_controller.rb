9b483512ab4b752d6f678227d2cef909410ecc27
Sophie Lejeune <sophie.shadi.lejeune@gmail.com>
2019-11-12 13:52:05 +1100

    finished the tasks

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,42 @@
+class TasksController < ApplicationController
+  before_action :find_by_id, only: [:show, :edit, :update, :destroy]
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path
+  end
+
+  def edit
+  end
+
+  def update
+    @task.update(task_params)
+    redirect_to task_path
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def find_by_id
+    @task = Task.find(params[:id])
+  end
+end
